95e1282c8b9488d3ca9854f1f75942676308b5ec
AlexG1410 <alexd.gutierrezj@uqvirtual.edu.co>
2024-10-10 17:16:01 -0500

    rama3

---
+++ src/main/java/co/edu/uniquindio/proyectofinalbancouq/model/Categoria.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.proyectofinalbancouq.model;
+
+public class Categoria {
+
+}
---
+++ src/main/java/co/edu/uniquindio/proyectofinalbancouq/model/Cuenta.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.proyectofinalbancouq.model;
+
+public class Cuenta {
+
+    private  String idCuenta;
+    private String nombreBanco;
+    private
+}
