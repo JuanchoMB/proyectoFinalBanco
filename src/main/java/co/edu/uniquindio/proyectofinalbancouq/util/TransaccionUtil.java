575cda9d76025c765a1f11794c79f00740c4bce1
AlexG1410 <alexd.gutierrezj@uqvirtual.edu.co>
2024-10-16 20:30:10 -0500

    rama5

--- src/main/java/co/edu/uniquindio/proyectofinalbancouq/util/TransaccionUtil.java
+++ src/main/java/co/edu/uniquindio/proyectofinalbancouq/util/TransaccionUtil.java
@@ -10,29 +10,22 @@ import java.time.format.DateTimeFormatter;
 public class TransaccionUtil {
 
     private static final String TRANSACCION_PATH = "C:/td/persistencia/transacciones.txt";
-    private static final String RESPALDO_PATH = "C:/td/persistencia/respaldo/transacciones_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + ".txt";
+
 
     // Guardar una nueva transacción
     public static void guardarTransaccion(Transaccion transaccion) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(TRANSACCION_PATH, true))) {
             writer.write(transaccion.getIdTransaccion() + "@@" + transaccion.getCantidad() + "@@" +
                     transaccion.getTipo() + "@@" + transaccion.getFecha() + "\n");
-            respaldarTransaccion();
-        } catch (IOException e) {
+
+        }
+
+        catch (IOException e) {
             LogUtil.registrarExcepcion(e);
         }
     }
 
     // Realizar respaldo
-    private static void respaldarTransaccion() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(TRANSACCION_PATH));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(RESPALDO_PATH))) {
-            String linea;
-            while ((linea = reader.readLine()) != null) {
-                writer.write(linea + "\n");
-            }
-        } catch (IOException e) {
-            LogUtil.registrarExcepcion(e);
-        }
+
     }
-}
+
